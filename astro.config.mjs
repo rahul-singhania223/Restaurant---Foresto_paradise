cdc8bfea6e0f0b2e7a0173f15481e1a25d765180
rahul-singhania223 <rahul4software@outlook.com>
2026-09-15 17:23:20 +0530

    moved components from pages folder for the sake of build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  image: {
+    domains: ["images.unsplash.com"],
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
